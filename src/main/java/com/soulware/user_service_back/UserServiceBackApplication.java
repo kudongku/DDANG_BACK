cdc970e14cb8549b4b245fdfdabc4cf34edd82b6
peter <peter@soulware.kr>
2025-01-20 13:50:43 +0900

    feat: init commit

---
+++ src/main/java/com/soulware/user_service_back/UserServiceBackApplication.java
@@ -0,0 +1,14 @@
+package com.soulware.user_service_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserServiceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soulware/user_service_back/UserServiceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulware.user_service_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
